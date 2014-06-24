990a172d01d7a88a364c5f0f0c90441748a3752e
tkisch <tkisch@users.noreply.github.com>
2014-06-24 13:07:24 +0200

    Update usefullfunctions.R

--- usefullfunctions.R
+++ usefullfunctions.R
@@ -1,15 +1,16 @@
+###########################################################
 # check if packages are installed, if yes require, if no install
 
-pkg <- c("sfsmisc","MASS","nnet","regr0")
-as.matrix(pkg)
+# pkg <- c("sfsmisc","MASS","nnet","regr0")
+# as.matrix(pkg)
 
 inst <- function(x){
   require(x) || install.packages(x)
   
 }
 
-apply(as.matrix(pkg), 2, inst)
+# apply(as.matrix(pkg), 2, inst)
 
+###########################################################
 # regr0 package (W.Stahel ETHZ)
-
-source("http://stat.ethz.ch/~stahel/regression/regr.R")
+# source("http://stat.ethz.ch/~stahel/regression/regr.R")
